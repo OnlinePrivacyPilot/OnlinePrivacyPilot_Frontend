89bf3aacb9368bf7a5e6c445fcb24868c3c355ce
b3noix <benoit.leconte@grenoble-inp.org>
2023-04-27 10:38:40 +0200

    Works better

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Description from './Description';
 
 
 class Banner extends Component {
@@ -8,8 +6,20 @@ class Banner extends Component {
     render() { 
         return (
         <div className='w-screen h-[80px] z-10 bg-zinc-400 fixed drop-shalow-lg' >
-            <Header />
-            <Description />
+            <div className='toto font-bold mr-4 text-4xl'>OnlinePrivacyPilot</div>
+            <div className='bannerlist'>
+            <ul className='hiden md:flex'>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Documentation
+                </li>
+                <li>
+                    About
+                </li>
+            </ul>
+        </div>
         </div>);
     }
 }
