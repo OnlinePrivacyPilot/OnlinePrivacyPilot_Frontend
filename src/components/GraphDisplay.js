207d6ecd300dc9a25147175e483d1e6e73266149
b3noix <benoit.leconte@grenoble-inp.org>
2023-05-04 12:29:22 +0200

    graph display near responsive, TBD center buttons

--- src/components/GraphDisplay.js
+++ src/components/GraphDisplay.js
@@ -52,14 +52,14 @@ export const GraphDisplay = (props) => {
     };
 
   return (
-    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <a href="#" className={`${showGraph === true? 'bg-indigo-600 text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-sm font-semibold' : 'text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'} rounded-md border relative z-10 inline-flex items-center px-4 py-2`} onClick={handleGraphClick}>Graph</a>
-        <a href="#" className={`${showGraph === false? 'bg-indigo-600 text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-sm font-semibold' : 'text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'} rounded-md border relative z-10 inline-flex items-center px-4 py-2`} onClick={handleListClick}>List</a>
+    <div className="flex flex-wrap items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="inline-flex -space-x-px justify-around sm:hidden p-2">
+        <a href="#" className={`${showGraph === true? 'bg-indigo-300 text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-sm font-semibold' : 'text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'} rounded-md border relative z-10 inline-flex items-center px-4 py-2`} onClick={handleGraphClick}>Graph</a>
+        <a href="#" className={`${showGraph === false? 'bg-indigo-300 text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-sm font-semibold' : 'text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'} rounded-md border relative z-10 inline-flex items-center px-4 py-2`} onClick={handleListClick}>List</a>
       </div>
       
-      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-        <div className="w-full m-4">
+      <div className="flex-wrap w-full justify-between">
+        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between w-full m-4">
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
             <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" onClick={() => setShowGraph(showGraph => !showGraph)}>
               <span className="sr-only">Graph</span>
@@ -76,11 +76,10 @@ export const GraphDisplay = (props) => {
                 <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
               </svg>
             </a>
-          </nav>
-          <div className="m-4">
+          </nav>          
+        </div>
+        <div className="m-4">
             {renderPageContent()}
-          </div>
-          
         </div>
       </div>
     </div>
