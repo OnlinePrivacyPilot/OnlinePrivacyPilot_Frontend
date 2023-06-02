dc8610bd13a437360409dd77b5a85151d1a82894
mablr <mablr@pm.me>
2023-06-02 15:32:13 +0200

    Adding blur effect behind the modal

--- src/components/DisplayGraph.js
+++ src/components/DisplayGraph.js
@@ -85,7 +85,7 @@ export function DisplayGraph({fingerprint}) {
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                     >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
+                        <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm" />
                     </Transition.Child>
 
                     <div className="fixed inset-0 overflow-y-auto">
